4b104069a8d64e5c14f17427c1bc96158c9376ea
MarioRoncador <Mario@Marios-MBP.home>
2016-11-11 22:34:34 -0500

    Angular Assignment 6 complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,8 +1,20 @@
  (function() {
      function AlbumCtrl() {
+         this.headline_text = "This works!";
+         
+         this.albumData = angular.copy(albumBig);
+         
+         this.song_list = [
+         { title: 'Hypnotize', duration: '4:01' },
+         { title: 'Fuck You Tonight', duration: '5:24' },
+         { title: 'I Love The Dough', duration: '3:45'},
+         { title: 'I Got A Story To Tell', duration: '3:18' },
+         { title: 'Kick In The Door', duration: '3:15'}
+     ]
          
      }
      angular
          .module('blocJams')
          .controller('AlbumCtrl', AlbumCtrl);
  })();
+

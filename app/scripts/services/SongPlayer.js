d9bf0807687b020426fe94cff62f2bfa3605fa1d
MarioRoncador <Mario@Marios-MBP.home>
2017-01-21 22:06:47 -0500

    So far so good

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
          
           /**
@@ -30,6 +30,12 @@
                preload: true
             });
  
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+              
             SongPlayer.currentSong = song;
           };
          
@@ -68,6 +74,12 @@
          * @type {Object}
          */
       SongPlayer.currentSong = null;
+        
+         /**
+         * @desc Current playback time (in seconds) of currently playing song
+         * @type {Number}
+         */
+      SongPlayer.currentTime = null;
          
       SongPlayer.play = function(song) {
          song = song || SongPlayer.currentSong;
@@ -125,6 +137,17 @@
              playSong(song);
          }
       };
+         
+      /**
+      * @function setCurrentTime
+      * @desc Set current time (in seconds) of currently playing song
+      * @param {Number} time
+      */
+      SongPlayer.setCurrentTime = function(time) {
+          if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+          }
+      };
 
           return SongPlayer;
      }
@@ -133,5 +156,5 @@
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
